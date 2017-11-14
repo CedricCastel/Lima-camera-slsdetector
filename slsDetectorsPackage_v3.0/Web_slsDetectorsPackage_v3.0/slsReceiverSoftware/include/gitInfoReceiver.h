be6eec3c70545470db646f38a700bb4dda585043
CedricCastel <cedric.castel@synchrotron-soleil.fr>
2017-11-14 11:03:09 +0100

    Sls

---
+++ slsDetectorsPackage_v3.0/Web_slsDetectorsPackage_v3.0/slsReceiverSoftware/include/gitInfoReceiver.h
@@ -0,0 +1,11 @@
+//#define SVNPATH ""
+#define SVNURL "git@git.psi.ch:sls_detectors_software/sls_receiver_software.git"
+//#define SVNREPPATH ""
+#define SVNREPUUID "ff2218ac1220963dfbacef2f474a456d05f518de"
+//#define SVNREV 0x664
+//#define SVNKIND ""
+//#define SVNSCHED ""
+#define SVNAUTH "Dhanya_Maliakal"
+#define SVNREV 0x664
+#define SVNDATE 0x20171019
+//
