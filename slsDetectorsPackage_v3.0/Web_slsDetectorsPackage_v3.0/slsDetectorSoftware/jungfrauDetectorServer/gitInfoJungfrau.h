40fb3657c8c9d5fc10f9729bfc9761e486e836e7
CedricCastel <cedric.castel@synchrotron-soleil.fr>
2017-11-14 11:02:58 +0100

    Sls

---
+++ slsDetectorsPackage_v3.0/Web_slsDetectorsPackage_v3.0/slsDetectorSoftware/jungfrauDetectorServer/gitInfoJungfrau.h
@@ -0,0 +1,11 @@
+//#define SVNPATH ""
+#define SVNURL "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
+//#define SVNREPPATH ""
+#define SVNREPUUID "c58dfe801770ceb63a9baab0be5b4cd16794b57e"
+//#define SVNREV 0x1495
+//#define SVNKIND ""
+//#define SVNSCHED ""
+#define SVNAUTH "Dhanya_Maliakal"
+#define SVNREV 0x1495
+#define SVNDATE 0x20170815
+//
---
+++ slsDetectorsPackage_v3.0/Web_slsDetectorsPackage_v3.0/slsDetectorSoftware/slsDetector/gitInfoLib.h
@@ -0,0 +1,11 @@
+//#define SVNPATH ""
+#define SVNURLLIB "git@git.psi.ch:sls_detectors_software/sls_detector_software.git"
+//#define SVNREPPATH ""
+#define SVNREPUUIDLIB "3c1eb747d1930d6d38030a5a607f72d3b58a7a21"
+//#define SVNREV 0x1549
+//#define SVNKIND ""
+//#define SVNSCHED ""
+#define SVNAUTHLIB "Dhanya_Maliakal"
+#define SVNREVLIB 0x1549
+#define SVNDATELIB 0x20171103
+//
